bbb9e27ffa407a4fa0ce42a9ea18c1c37040b7af
TechnoLustMatty <130214281+TechnoLustMatty@users.noreply.github.com>
2023-04-14 03:42:58 -0400

    Update listening.js

--- statuses/listening.js
+++ statuses/listening.js
@@ -1,10 +1,12 @@
+easier to read
 const
   rpcGenerator = require("discordrpcgenerator"),
 
   // Add your client id in the index.js file
   IMAGE = "The name of the image",
   SONG = "The name of the song",
-  ARTIST = "The artist of the song";
+  ARTIST = "The artist of the song"
+  URL = "The URL of song ig"; // Credits to BCM for this single line of code
 
 module.exports = (client, CLIENT_ID) => rpcGenerator.getRpcImage(CLIENT_ID, IMAGE)
   .then(image => client.user.setPresence(
@@ -13,5 +15,6 @@ module.exports = (client, CLIENT_ID) => rpcGenerator.getRpcImage(CLIENT_ID, IMAG
       .setAssetsLargeImage(image.id)
       .setDetails(SONG)
       .setState(ARTIST)
+      .setUrl(URL) // And this too ;)
       .toDiscord()
-  ));
+  )); 
